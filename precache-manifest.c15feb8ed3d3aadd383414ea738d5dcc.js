4228a28d7c7007e7b5f61697ad618985f98fec91
younessbennaim <bennaimyouness@gmail.com>
2020-12-30 20:17:54 +0100

    Updates

---
+++ precache-manifest.c15feb8ed3d3aadd383414ea738d5dcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24273d7bdc31022aa515e7e7ee47e5eb",
+    "url": "/CoronaCases/index.html"
+  },
+  {
+    "revision": "fa609afffcbbbcd98ce8",
+    "url": "/CoronaCases/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6e45ab6b10d35279697c",
+    "url": "/CoronaCases/static/css/main.35591d24.chunk.css"
+  },
+  {
+    "revision": "fa609afffcbbbcd98ce8",
+    "url": "/CoronaCases/static/js/2.7ca7cd1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaCases/static/js/2.7ca7cd1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e45ab6b10d35279697c",
+    "url": "/CoronaCases/static/js/main.e4201a20.chunk.js"
+  },
+  {
+    "revision": "04d6f5636f54f244fd8b",
+    "url": "/CoronaCases/static/js/runtime-main.3cca3663.js"
+  },
+  {
+    "revision": "a82040ecf06139df2f3e653326b3420c",
+    "url": "/CoronaCases/static/media/Bangers-Regular.a82040ec.ttf"
+  }
+]);
